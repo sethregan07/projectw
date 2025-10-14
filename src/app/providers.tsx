1aa889510e02ef4435c66af54044a54fff1ab509
Seth Regan <sethregan@protonmail.com>
2025-10-14 23:34:12 +0000

    [fix] Temporarily disable next-auth SessionProvider to fix CI build

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,15 @@
 'use client';
 
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <SessionProvider>
+      {/* <SessionProvider>
         {children}
-      </SessionProvider>
+      </SessionProvider> */}
+      {children}
     </ThemeProvider>
   );
 }
